4d3c67199f3757bb6f8a8391974c9ad6b2c3bbb0
Claudiu Sarb <sarb.claudiu391@gmail.com>
2020-04-27 20:30:31 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,16 @@
 # Add your variables here
+first_number = 4
+second_number = 2
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+# def first_number
+# end
+#
+# def second_number
+# end
+#
+# def sum
+# end
